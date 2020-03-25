d7d6fa7cd9c56254c8ab6e8701134da4d0c11d0d
trtincher <trtincher12@gmail.com>
2020-03-25 16:12:15 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log("User flipped "+cardOne)
-console.log("User flipped "+cardFour)
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen")
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You fond a match!")
+	}
+	else{
+		alert("Sorry try again")
+	}
+}
