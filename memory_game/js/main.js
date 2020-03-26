ce93c0a84bee4eed158b651c13cedd55c95bc00d
trtincher <trtincher12@gmail.com>
2020-03-26 11:37:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,24 @@
-console.log("Up and running!");
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen")
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You fond a match!")
-	}
-	else{
-		alert("Sorry try again")
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}
+		else{
+			alert("Sorry try again");
+		}
 	}
 }
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
